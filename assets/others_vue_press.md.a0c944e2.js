b4cb1828dbb7e3a4e8e5e86ef63530433570207c
j <2019292338@qq.com>
2022-01-15 16:18:54 +0800

    deploy

--- assets/others_vue_press.md.a0c944e2.js
+++ assets/others_vue_press.md.a0c944e2.js
@@ -1,4 +1,4 @@
-import{_ as n,c as s,o as a,a as t}from"./app.a1ad4963.js";const m='{"title":"\u90E8\u7F72\u5230 github page","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u90E8\u7F72\u5230 github page","slug":"\u90E8\u7F72\u5230-github-page"}],"relativePath":"others/vue_press.md","lastUpdated":1642233787080}',e={},p=t(`<h2 id="\u90E8\u7F72\u5230-github-page" tabindex="-1">\u90E8\u7F72\u5230 github page <a class="header-anchor" href="#\u90E8\u7F72\u5230-github-page" aria-hidden="true">#</a></h2><p>\u5728 git \u4E2D\u65B0\u589E\u5982\u4E0B\u94A9\u5B50\u5373\u53EF</p><div class="language-shell"><pre><code><span class="token shebang important">#!/bin/bash</span>
+import{_ as n,c as s,o as a,a as t}from"./app.a1ad4963.js";const m='{"title":"\u90E8\u7F72\u5230 github page","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u90E8\u7F72\u5230 github page","slug":"\u90E8\u7F72\u5230-github-page"}],"relativePath":"others/vue_press.md","lastUpdated":1642234662490}',e={},p=t(`<h2 id="\u90E8\u7F72\u5230-github-page" tabindex="-1">\u90E8\u7F72\u5230 github page <a class="header-anchor" href="#\u90E8\u7F72\u5230-github-page" aria-hidden="true">#</a></h2><p>\u5728 git \u4E2D\u65B0\u589E\u5982\u4E0B\u94A9\u5B50\u5373\u53EF</p><div class="language-shell"><pre><code><span class="token shebang important">#!/bin/bash</span>
 
 <span class="token comment"># \u786E\u4FDD\u811A\u672C\u629B\u51FA\u9047\u5230\u7684\u9519\u8BEF</span>
 <span class="token builtin class-name">set</span> -e
@@ -6,7 +6,6 @@ import{_ as n,c as s,o as a,a as t}from"./app.a1ad4963.js";const m='{"title":"\u
 <span class="token comment"># \u751F\u6210\u9759\u6001\u6587\u4EF6</span>
 <span class="token assign-left variable">file</span><span class="token operator">=</span><span class="token string">&quot;docs/.vitepress/dist&quot;</span>
 
-<span class="token comment"># \u5C06\u65E0\u7528\u6587\u4EF6\u5148\u5220\u9664</span>
 <span class="token keyword">if</span> <span class="token punctuation">[</span> -f <span class="token variable">$file</span> <span class="token punctuation">]</span> <span class="token punctuation">;</span> <span class="token keyword">then</span>
     <span class="token function">rm</span> -rf <span class="token variable">$file</span>
 <span class="token keyword">fi</span>
@@ -22,7 +21,8 @@ import{_ as n,c as s,o as a,a as t}from"./app.a1ad4963.js";const m='{"title":"\u
 
 <span class="token comment"># \u672C\u5730\u90E8\u7F72</span>
 <span class="token builtin class-name">cd</span> <span class="token punctuation">..</span>
-<span class="token function">cp</span> -r dist/. d:/nginx/html
+<span class="token function">chmod</span> <span class="token number">777</span> d:/nginx/html
+<span class="token function">cp</span> -r dist/ d:/nginx/html
 <span class="token builtin class-name">cd</span> d:/nginx
 start nginx
 
